e987bee4750e66ca2a577aeccc4320cae862ac2d
Robert Cooper <robertjscooper@gmail.com>
2025-06-12 10:15:37 +0100

    remove spaces

--- .github/workflows/docker_build.sh
+++ .github/workflows/docker_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
 trap 'exit' ERR
-TAG_NAME = "${BRANCH_NAME}"
+TAG_NAME="${BRANCH_NAME}"
 if [ "${BRANCH_NAME}" == "main"]; then
-    TAG_NAME = "latest"
+    TAG_NAME="latest"
 fi
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USER}" --password-stdin
 docker build -q -t ${GIT_REPO}:$TAG_NAME  .
