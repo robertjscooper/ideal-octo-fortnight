181e6156eaade060a03d05d5582df41365c26616
Robert Cooper <robertjscooper@gmail.com>
2025-06-12 10:26:46 +0100

    test quotes

--- .github/workflows/docker_build.sh
+++ .github/workflows/docker_build.sh
@@ -2,9 +2,9 @@
 set -ev
 trap 'exit' ERR
 TAG_NAME="${BRANCH_NAME}"
-if [ "${BRANCH_NAME}" == "main"]; then
+if [ "${BRANCH_NAME}" == "main" ]; then
     TAG_NAME="latest"
 fi
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USER}" --password-stdin
-docker build -q -t ${GIT_REPO}:$TAG_NAME  .
-docker push ${GIT_REPO}:$TAG_NAME
+docker build -q -t "${GIT_REPO}:$TAG_NAME"  .
+docker push "${GIT_REPO}:$TAG_NAME"
