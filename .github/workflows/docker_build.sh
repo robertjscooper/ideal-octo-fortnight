65bca53f50014c330e0c8c974ad24a82e97a5adf
Robert Cooper <robertjscooper@gmail.com>
2025-06-12 09:49:48 +0100

    add docker push

--- .github/workflows/docker_build.sh
+++ .github/workflows/docker_build.sh
@@ -4,3 +4,4 @@ trap 'exit' ERR
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USER}" --password-stdin
 docker build -q -t robertjscooper/ideal-octo-fortnight  .
+docker push robertjscooper/ideal-octo-fortnight
