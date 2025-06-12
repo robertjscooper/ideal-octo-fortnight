69a25da5cb60567c72a66fe83874825be85cd1e7
Robert Cooper <robertjscooper@gmail.com>
2025-06-12 17:01:42 +0100

    update

--- .github/workflows/docker_build.sh
+++ .github/workflows/docker_build.sh
@@ -7,6 +7,7 @@ docker build --network=host -t "${GIT_REPO}:latest"  .
 
 if [ "${PR_STATE}" == "Closed" ]; then
     COMMIT_MESSAGE=$(git log -1 --pretty=format:"%s")
+    IFS=' '
     read -ra newarr <<< "$COMMIT_MESSAGE"
     OLD_BRANCH=$(${newarr[5]})
     git config --local user.email "action@github.com"
@@ -23,13 +24,14 @@ if [ "${PR_STATE}" == "Closed" ]; then
     echo "Old Branch name: $OLD_BRANCH"
     echo "Git Tag will be: $GIT_TAG"
 
-    # docker push ${GIT_REPO}:$GIT_TAG
-    # git push origin "$GIT_TAG"
+    docker push ${GIT_REPO}:$GIT_TAG
+    git push origin "$GIT_TAG"
 
     TOKEN=`curl -s -H "Content-Type: application/json" -X POST -H "Content-Type: application/json" -d '{"username": "'$docker_user'", "password":"'$docker_pass'"}' "https://hub.docker.com/v2/users/login/" | jq -r .token`
-    IFS=' '
-    # curl "https://hub.docker.com/v2/repositories/${TRAVIS_REPO_SLUG}/tags/${newarr[5]}/" -X DELETE -H "Authorization: JWT ${TOKEN}"
+    curl "https://hub.docker.com/v2/repositories/${TRAVIS_REPO_SLUG}/tags/$OLD_BRANCH/" -X DELETE -H "Authorization: JWT ${TOKEN}"
 elif [ "${PR_STATE}" == "Open" ]; then
+    echo "PR State is: ${PR_STATE}"
+    echo "Branch Name is: ${BRANCH_NAME}"
     docker tag ${GIT_REPO}:latest ${GIT_REPO}:${BRANCH_NAME}
     docker push ${GIT_REPO}:${BRANCH_NAME}
 fi
